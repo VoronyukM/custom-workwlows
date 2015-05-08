87dc40f92fd8cffcca707d35b7f0cf7a3fed6aa1
Mikhail Voronyuk <VoronyukM@gmail.com>
2015-05-08 21:23:44 +0300

    random assignment of support issues: first version. Issue #1847

---
+++ random-assignment-of-support-issues.rb
@@ -0,0 +1,21 @@
+# check subject to except checking via custom_workflows checker
+# tracker = Support
+# assignee = -
+if @issue.subject.present? && @issue.tracker_id == 3 && !@issue.assigned_to_id
+  # assign the issue to any random Developer
+  role_to_select = Role.find_by_id(4)
+
+  users_to_select = @issue.project.users_by_role[role_to_select]
+
+  if users_to_select.size
+  # continue only if there is someone to whom we can assign the issue
+
+    prng = Random.new
+    rand_idx_inside_hash = prng.rand(users_to_select.size)
+    random_user = users_to_select[rand_idx_inside_hash]
+
+    @issue.assigned_to_id = random_user.id
+  end
+
+
+end
