0682ca16b15fb2b645b2a5995f58ede33193873e
Mikhail Voronyuk <Voronyuk.M@3soft.ru>
2016-06-08 12:53:38 +0300

    use standard plugin's export to xml for 'check if owner-email is filled when send_to_owner is checked'

--- check-if-owner-email-is-filled-when-send_to_owner-is-checked.rb
+++
@@ -1,4 +0,0 @@
-if !@current_journal.nil? && @current_journal.send_to_owner
-  owner_email = @issue.custom_field_value( CustomField.find_by_name('owner-email') )
-  raise WorkflowError, "It's impossible to send an email to nobody! Specify the owner's email or uncheck the flag 'Send mail to supportclient'" if owner_email.blank?
-end
